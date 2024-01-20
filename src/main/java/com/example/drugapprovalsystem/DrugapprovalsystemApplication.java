a876ffb7834e98d2b20e55cbbdae18cc64659788
Nguyễn Tấn <76957209+tanlionel@users.noreply.github.com>
2024-01-20 11:46:25 +0700

    first commit

---
+++ src/main/java/com/example/drugapprovalsystem/DrugapprovalsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.drugapprovalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugapprovalsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrugapprovalsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/drugapprovalsystem/DrugapprovalsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.drugapprovalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrugapprovalsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
