ccebc495608e461c198247f1132ae462ea236f3d
Nguyễn Tấn <76957209+tanlionel@users.noreply.github.com>
2024-01-22 12:11:21 +0700

    find user by email and fix bug

---
+++ src/main/java/com/example/drugapprovalsystem/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.example.drugapprovalsystem.controller;
+
+import com.example.drugapprovalsystem.entity.User;
+import com.example.drugapprovalsystem.exception.InvalidateException;
+import com.example.drugapprovalsystem.exception.UserDoesNotExistException;
+import com.example.drugapprovalsystem.model.DTO.UserResponseDTO;
+import com.example.drugapprovalsystem.service.ServiceInterface.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+@AllArgsConstructor
+@CrossOrigin
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/find-by-email")
+    public ResponseEntity<?> getUserByEmail(@RequestParam String email) throws UserDoesNotExistException, InvalidateException {
+        User user = userService.getUserByEmail(email);
+        return ResponseEntity.ok(UserResponseDTO.builder()
+                        .id(user.getId())
+                        .email(user.getEmail())
+                        .dayOfBirth(user.getDayOfBirth())
+                        .gender(user.getGender())
+                        .roleName(user.getRole().getName())
+                        .username(user.getUsername())
+                        .fullname(user.getFullname())
+                        .build());
+    }
+}
---
+++ src/main/java/com/example/drugapprovalsystem/model/DTO/UserResponseDTO.java
@@ -0,0 +1,23 @@
+package com.example.drugapprovalsystem.model.DTO;
+
+import com.example.drugapprovalsystem.entity.Role;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class UserResponseDTO {
+
+    private Integer id;
+    private String username;
+    private String email;
+    private String fullname;
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd")
+    private LocalDate dayOfBirth;
+    private Integer gender;
+    private String roleName;
+}
