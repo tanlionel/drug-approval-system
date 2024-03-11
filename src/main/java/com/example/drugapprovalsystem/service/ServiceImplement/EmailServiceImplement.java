f7292a89acf2d9bfc9d0ecd1a7fadf9c7cb2f3f2
Nguyễn Tấn <76957209+tanlionel@users.noreply.github.com>
2024-03-11 14:22:47 +0700

    done 90%

--- src/main/java/com/example/drugapprovalsystem/service/ServiceImplement/EmailServiceImplement.java
+++ src/main/java/com/example/drugapprovalsystem/service/ServiceImplement/EmailServiceImplement.java
@@ -1,6 +1,7 @@
 package com.example.drugapprovalsystem.service.ServiceImplement;
 
 import com.example.drugapprovalsystem.service.ServiceInterface.EmailService;
+import com.example.drugapprovalsystem.service.ServiceInterface.ThymeleafService;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,19 +9,31 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class EmailServiceImplement implements EmailService {
     @Value("${spring.mail.username}")
     private String fromEmail;
 
-
+    @Autowired
+    ThymeleafService thymeleafService;
     @Autowired
     private JavaMailSender javaMailSender;
+
+
 
     @Override
     public ResponseEntity<?> sendMail(String to) {
         try {
+            Map<String,Object> variables = new HashMap<>();
+            variables.put("content","System notification to admin, please log in to the pharmacy system to view prescription management.");
+            variables.put("image","https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/330px-Image_created_with_a_mobile_phone.png")   ;
+            variables.put("tesText","hehe");
             MimeMessage mimeMessage = javaMailSender.createMimeMessage();
             MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage,true);
             mimeMessageHelper.setFrom(fromEmail);
@@ -28,7 +41,7 @@ public class EmailServiceImplement implements EmailService {
 
             String subject = "test";
             mimeMessageHelper.setSubject(subject);
-            mimeMessageHelper.setText("test body");
+            mimeMessageHelper.setText(thymeleafService.createContent("email-template.html",variables),true);
 
             javaMailSender.send(mimeMessage);
 
---
+++ src/main/java/com/example/drugapprovalsystem/service/ServiceImplement/ThymeleafServiceImplement.java
@@ -0,0 +1,55 @@
+package com.example.drugapprovalsystem.service.ServiceImplement;
+
+import com.example.drugapprovalsystem.service.ServiceInterface.ThymeleafService;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import java.util.Map;
+
+@Service
+public class ThymeleafServiceImplement implements ThymeleafService {
+    private static final String MAIL_TEMPLATE_BASE_NAME = "mail/MailMessages";
+    private static final String MAIL_TEMPLATE_PREFIX = "/templates/";
+    private static final String MAIL_TEMPLATE_SUFFIX = ".html";
+    private static final String UTF_8 = "UTF-8";
+    private static TemplateEngine templateEngine;
+    static {
+        templateEngine = emailTemplateEngine();
+    }
+
+    private static TemplateEngine emailTemplateEngine() {
+        final SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateEngineMessageSource(emailMessageSource());
+        templateEngine.setTemplateResolver(htmlTemplatorResolver());
+        return templateEngine;
+    }
+
+    private static ITemplateResolver htmlTemplatorResolver() {
+        final ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix(MAIL_TEMPLATE_PREFIX);
+        templateResolver.setSuffix(MAIL_TEMPLATE_SUFFIX);
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding(UTF_8);
+        templateResolver.setCacheable(false);
+        return templateResolver;
+    }
+
+    private static ResourceBundleMessageSource emailMessageSource() {
+        final ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename(MAIL_TEMPLATE_BASE_NAME);
+        return messageSource;
+    }
+
+    @Override
+    public String createContent(String template, Map<String, Object> variables) {
+        final Context context = new Context();
+        context.setVariables(variables);
+        return templateEngine.process(template,context);
+    }
+}
