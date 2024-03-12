462557f1c22f892707ef3bd808d2cd18c2b8c930
Nguyễn Tấn <76957209+tanlionel@users.noreply.github.com>
2024-03-12 15:35:47 +0700

    Update ProfileResponseDTO.java

--- src/main/java/com/example/drugapprovalsystem/model/DTO/profile_response_dto/ProfileResponseDTO.java
+++ src/main/java/com/example/drugapprovalsystem/model/DTO/profile_response_dto/ProfileResponseDTO.java
@@ -1,6 +1,7 @@
 package com.example.drugapprovalsystem.model.DTO.profile_response_dto;
 
 import com.example.drugapprovalsystem.entity.User;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Column;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
@@ -18,9 +19,12 @@ import java.time.LocalDateTime;
 public class ProfileResponseDTO {
     private int profileId;
     private String title;
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd HH:mm:ss.SSS")
     private String createdBy;
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd HH:mm:ss.SSS")
     private LocalDateTime createdOn;
     private String updatedBy;
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd HH:mm:ss.SSS")
     private LocalDateTime updatedOn;
     private String status;
 }
